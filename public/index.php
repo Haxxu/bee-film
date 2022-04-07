98405937dc3e6833805928b0a8be73ee864ad8d2
Minh <nguyenngocminh190901@gmail.com>
2022-04-07 08:13:10 +0700

    Sua phan updated cho sql

--- public/index.php
+++ public/index.php
@@ -9,6 +9,11 @@
     <title>Bee Film</title>
     <!-- Link To CSS -->
     <?php include_once('./linkCSS.php'); ?>
+    <!-- Link Swiper's CSS -->
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/swiper/swiper-bundle.min.css"
+    />
 </head>
 <body>
 
@@ -19,13 +24,29 @@
 
         <!-- Content -->
         <div style="color: #fff; font-size: 1.5rem;">
-        <div>
+        <!-- <div>
           <?php
             if (isset($_SESSION['message'])) {
               echo "<h1>" . $_SESSION['message']['body'] . "</h1>";
               unset($_SESSION['message']);
             }
           ?>
+        </div> -->
+
+        <div class="swiper mySwiper">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">Slide 1</div>
+            <div class="swiper-slide">Slide 2</div>
+            <div class="swiper-slide">Slide 3</div>
+            <div class="swiper-slide">Slide 4</div>
+            <div class="swiper-slide">Slide 5</div>
+            <div class="swiper-slide">Slide 6</div>
+            <div class="swiper-slide">Slide 7</div>
+            <div class="swiper-slide">Slide 8</div>
+            <div class="swiper-slide">Slide 9</div>
+          </div>
+          <div class="swiper-button-next"></div>
+          <div class="swiper-button-prev"></div>
         </div>
         
         </div>
@@ -42,6 +63,17 @@
     
       </div>    
     
+    <!-- Link To Swiper's JS -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <!-- Script Swiper -->
+    <script>
+      var swiper = new Swiper(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+    </script>
     <!-- Link To JS -->
     <?php include_once('./linkJS.php'); ?>
 </body>
