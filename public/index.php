ec9b86b4c99b639c832abdb6bbe3dfad4dd78cec
Haxu <nguyenngocminh190901@gmail.com>
2022-04-08 12:02:37 +0700

    doi owl carousel thanh swiper

--- public/index.php
+++ public/index.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bee Film</title>
-    <!-- Link To OWL CAROUSEL CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Link to Swiper's CSS -->
+    <link rel="stylesheet" href="./assets/css/swiper-bundle.css">
     <!-- Link To CSS -->
     <?php include_once('./linkCSS.php'); ?>
 
@@ -20,47 +20,36 @@
         <!-- Header -->
         <?php include_once("./header.php"); ?>
 
-        <!-- Show Message Toast -->
-        <!-- <div style="color: #fff; font-size: 1.5rem;">
-            <?php
-              if (isset($_SESSION['message'])) {
-                echo "<h1>" . $_SESSION['message']['body'] . "</h1>";
-                unset($_SESSION['message']);
-              }
-            ?>
-        </div> -->
         
         <!-- Content -->
         <div class="home container">
-          <!-- HERO SECTION -->
-          <div class="hero-section">
-            <div class="owl-carousel carousel-nav-center" id="hero-carousel">
-              <!-- SLIDE ITEM -->
-              <div class="hero-slide-item">
-                <img src="./assets/images/black-banner.png" alt="">
-                <div class="overlay"></div>
-                <div class="hero-slide-item-content">
-                  <div class="item-content-wrapper">
-                    <div class="item-content-title top-down">
-                      Black Panther
-                    </div>
-                    <div class="film-infos top-down delay-2">
-                      
+          <!-- HERO SLIDE -->
+          <section class="hero-swiper swiper">
+            <div class="swiper-wrapper">
+              <!-- Box 1 -->
+              <div class="swiper-slide">
+                <div class="hero-item">
+                  <img src="./assets/images/black-banner.png" alt="">
+                  <div class="hero-item-text">
+                    <h1 id="cay-vl">Black Panther</h1>
+                    <h3>Báo đen</h3>
+                    <div class="hero-item-status">
+                      <span>Hoàn tất</span>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </section>
         </div>
 
         <!-- Footer -->
         <?php include_once('./footer.php'); ?>
     
-      </div>    
+    </div>    
     
-    <!-- Link to OWL CAROUSEL JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Link to Swiper's JS -->
+    <script src="./assets/js/swiper-bundle.js"></script>
     <!-- Link To JS -->
     <?php include_once('./linkJS.php'); ?>
 </body>
--- public/linkCSS.php
+++ public/linkCSS.php
@@ -1,8 +1,8 @@
 <!-- Normalize CSS -->
-<link rel="stylesheet" href="./assets/css/normalize.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/normalize.css">
 <!-- Link to Bootstrap -->
-<link rel="stylesheet" href="./assets/bootstrap-5.1.3-dist/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="./assets/bootstrap-5.1.3-dist/css/bootstrap.css">
 <!-- Link to base CSS -->
-<link rel="stylesheet" href="./assets/css/base.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/base.css">
 <!-- Link to main CSS -->
-<link rel="stylesheet" href="./assets/css/main.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/main.css">
