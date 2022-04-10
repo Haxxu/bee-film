0d756454375aba22176d75d1e2988a5f82578059
Haxu <nguyenngocminh190901@gmail.com>
2022-04-10 12:26:16 +0700

    aaa

--- public/detail_film.php
+++ public/detail_film.php
@@ -15,6 +15,7 @@
 
         <div class="container">
             <div class="film-detail">
+                <!-- Breadcumb -->
                 <div class="film-detail-breadcrumb">
                     <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
                         <ol class="breadcrumb">
@@ -23,8 +24,11 @@
                         </ol>
                     </nav>
                 </div>  
+                <!-- End Breadcumb -->
+
+                <!-- Main Info -->
                 <div class="film-detail-main">
-                    <a href="./index.php" class="play-film">
+                    <a href="#trailer" class="play-film">
                         <i class='bx bx-play' ></i>
                     </a>
                     <img src="./assets/images/wanda-banner.jpg" alt="" class="film-img-banner">
@@ -32,7 +36,7 @@
                         <div class="film-img-poster-box">
                             <img src="./assets/images/bat-man.jpg" alt="" class="film-img-poster">
                         </div>
-                        <div class="film-info-text">
+                        <div class="film-detail-main-text">
                             <div class="film-title">
                                 Bóng ma anh quốc
                             </div>
@@ -51,7 +55,102 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <!-- End Main Info -->
 
+                <!-- Film Info -->
+                <div class="film-detail-box container">
+                    <!-- Rating -->
+                    <div class="film-detail-title">
+                        Đánh giá phim
+                    </div>
+                    <div class="film-rating">
+                        <div class="film-rating-star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bx-star' ></i>
+                        </div>
+                        <div class="film-rating-score">
+                            <span>8.5 / 100 lượt</span>
+                        </div>
+                    </div>
+                    <!-- End Rating -->
+                    <!-- Detail text -->
+                    <div class="film-detail-text row">
+                        <div class="col-12 col-md-4 align-self-start">
+                            <div class="film-genre">
+                                <strong>Thể Loại:</strong> 
+                                <a href="">Phim hành động,</a>
+                                <a href="">Phim viễn tưởng,</a>
+                                <a href="">Phim viễn tưởng,</a>
+                                <a href="">Phim viễn tưởng,</a>
+                                <a href="">Phim viễn tưởng,</a>
+                                <a href="">Phim viễn tưởng,</a>  
+                            </div>
+                            <div class="film-status">
+                                <strong>Trạng thái: </strong>
+                                <span>Hoàn thành</span>
+                            </div>
+                            <div class="film-time">
+                                <strong>Thời lượng: </strong>
+                                <span>123 phút</span>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 align-self-start">
+                            <div class="film-year">
+                                <strong>Năm phát hành:</strong>
+                                <a href="">2019</a>
+                            </div>
+                            <div class="film-nation">
+                                <strong>Quốc gia:</strong>
+                                <a href="">Mỹ</a>
+                            </div>
+                            <div class="film-imdb">
+                                <strong>IMDB:</strong>
+                                <span class="badge bg-warning">8.5 / 10</span>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 align-self-start">
+                            <div class="film-director">
+                                <strong>Đạo diễn:</strong>
+                                <a href="">Nguyễn Ngọc Minh</a>
+                            </div>
+                            <div class="film-actor">
+                                <strong>Diễn viên:</strong>
+                                <a href="#">Nguyễn Ngọc Minh</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Detail text -->
+                    <hr>
+                    <!-- Film Description -->
+                    <div class="film-description">
+                        <div class="film-detail-title">
+                            Nội dung phim Thần điêu đại hiệp Bóng ma anh quốc
+                        </div>
+                        <div class="film-description-text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas recusandae mollitia maiores, vel libero, ratione adipisci dignissimos, eum provident natus perspiciatis beatae nam modi laborum iusto. Obcaecati minima delectus id.</p>
+                        </div>
+                    </div>
+                    <!-- End Film Description -->
+                    <hr>
+                    <!-- Trailer -->
+                    <div class="film-trailer">
+                        <div class="film-detail-title">
+                            Trailer
+                        </div>
+                        <div class="film-trailer-box">
+                            <iframe id="trailer" width="100%" height="600px" src="https://www.youtube.com/embed/TEd1012DT0I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <!-- End Trailer -->
                 </div>
             </div>
         </div>
